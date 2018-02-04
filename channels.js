31db2adc6f82e9c4ce7359fb99ceaa9bfa4b4312
Spencer Charest <SpencerC77@gmail.com>
2018-02-04 11:17:14 -0800

    Init commit

---
+++ channels.js
@@ -0,0 +1,10 @@
+const channels = [
+  "NESN HD",
+  "NESN Plus HD",
+  "ESPN HD",
+  "ESPN 2 HD",
+  "ESPN U HD",
+  "ESPNews HD"
+]
+
+module.exports = channels;
---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const rp = require('request-promise');
+const dotenv = require('dotenv').config();
+const wanted = require('./channels');
+
+const app = express();
+
+const username = process.env.USERNAME;
+const password = process.env.PASSWORD;
+
+app.get('/', (req, res) => {
+
+  rp({
+    method: 'GET',
+    url: `http://api.mystreams.club/vget?password=${password}&username=${username}`
+  })
+    .then((channels) => {
+
+      channels = channels.split('#EXTINF:-1 ');
+
+      channels = channels.filter((channel) => {
+        let include = false;
+        wanted.forEach((want) => {
+          if (channel.includes(want)) {
+            include = true;
+          }
+        });
+
+        return include;
+      });
+
+      channels = channels.map(channel => `#EXTINF:-1 container="mp2ts" ${channel}`);
+
+      const playlist = `#EXTM3U\n ${channels.join("")}\n`;
+
+      res.send(playlist);
+    })
+    .catch((err) => {
+      res.status(400).send({
+        message: 'Something went wrong...'
+      })
+    });
+});
+
+app.listen(7500, () => {
+  console.log('App listenting on port 7500!');
+});
